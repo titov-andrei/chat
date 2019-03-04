c32d0e30a28bdaaf192d41d948b93af0d7493f09
titov-andrei <titov_andrei@mail.ru>
2019-03-04 10:02:19 +0300

    Update Chat.php

--- classes/Chat.php
+++ classes/Chat.php
@@ -12,6 +12,6 @@ class Chat
       }
     }
     $key = $headers['Sec-WebSocket-Key'];
-    $sKey = base64_encode(pack('H*'));
+    $sKey = base64_encode(pack('H*', sha1($key.'258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
   }
 }
