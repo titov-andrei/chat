9ca0b8c6521301ed5f8da6944deceacee6fe3a91
titov-andrei <titov_andrei@mail.ru>
2019-03-09 07:18:16 +0300

    Update server.php

--- server.php
+++ server.php
@@ -4,4 +4,6 @@ require_once ("classes/Chat.php");
 
 $chat = new Chat();
 
-$socket = socket_create();
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socet_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
+socket_bind();
