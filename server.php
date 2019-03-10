01b03d959fa2636936869d7dc6cc503ad97d9fe8
titov-andrei <titov_andrei@mail.ru>
2019-03-10 11:39:32 +0300

    Update server.php

--- server.php
+++ server.php
@@ -1,9 +1,13 @@
 <?php
+define('PORT', "8090");
 
 require_once ("classes/Chat.php");
 
 $chat = new Chat();
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
 socet_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
-socket_bind();
+socket_bind($socket, 0, PORT);
+
+socket_listen($socket);
