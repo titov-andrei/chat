01ecdcc8787bf1c19651c3c37cad18e7c4878e8f
titov-andrei <titov_andrei@mail.ru>
2019-03-13 17:44:13 +0300

    Update Chat.php

--- classes/Chat.php
+++ classes/Chat.php
@@ -14,8 +14,13 @@ class Chat
     $key = $headers['Sec-WebSocket-Key'];
     $sKey = base64_encode(pack('H*', sha1($key.'258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
 
-    $strHeadr = "
+    $strHeadr = "HTTP/1.1 101 Switching Protocols \r\n" .
+    "Upgrade: websocket\r\n" .
+    "Connection: Upgrade\r\n" .
+    "WebSocket-Origin: $host\r\n" .
+    "WebSocket-Location: ws://$host:$port/chat/server.php\r\n".
+    "Sec-WebSocket-Accept:$sKey\r\n\r\n";;
     
-    ";
+    ;
   }
 }
